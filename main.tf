84b84ca8b79876289ad99dbcfc9599fcbf6f48eb
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2020-11-24 10:24:35 +0000

    RWA-197: Upgrade azurerm to v2 (#3)
    * Require terraform 0.13.
    * Upgrade azurerm to v2
    * Added new common_tags variable

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "azurerm" {
-  version = "1.44.0"
+  features {}
 }
 
 locals {
   common_tags = {
-    "environment"  = "${var.env}"
-    "Team Name"    = "${var.team_name}"
-    "Team Contact" = "${var.team_contact}"
-    "Destroy Me"   = "${var.destroy_me}"
+    "environment"  = var.env
+    "Team Name"    = var.team_name
+    "Team Contact" = var.team_contact
+    "Destroy Me"   = var.destroy_me
   }
 
-  managed_identity_object_id = "${var.managed_identity_object_id}"
+  managed_identity_object_id = var.managed_identity_object_id
 }
--- variables.tf
+++ variables.tf
@@ -1,48 +1,48 @@
 variable "product" {
-  type        = "string"
+  type        = string
   default     = "wa"
   description = "The name of your application"
 }
 
 variable "env" {
-  type        = "string"
+  type        = string
   description = "The deployment environment (sandbox, aat, prod etc..)"
 }
 
 variable "location" {
-  type    = "string"
+  type    = string
   default = "UK South"
 }
 
 variable "tenant_id" {
-  type        = "string"
+  type        = string
   description = "The Tenant ID of the Azure Active Directory"
 }
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
+  type        = string
   description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
 }
 
 variable "wa_product_group_object_id" {
-  type    = "string"
+  type    = string
   default = "cdeb331b-adfe-46a7-a2c8-a628e2d35d96"
 }
 
 variable "team_name" {
-  type        = "string"
+  type        = string
   description = "Team name"
   default     = "Work Allocation"
 }
 
 variable "team_contact" {
-  type        = "string"
+  type        = string
   description = "Team contact"
   default     = "#workallocation"
 }
 
 variable "destroy_me" {
-  type        = "string"
+  type        = string
   description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
   default     = "No"
 }
@@ -53,14 +53,18 @@ variable "managed_identity_object_id" {
 
 // as of now, UK South is unavailable for Application Insights
 variable "appinsights_location" {
-  type        = "string"
+  type        = string
   default     = "West Europe"
   description = "Location for Application Insights"
 
 }
 
 variable "appinsights_application_type" {
-  type        = "string"
+  type        = string
   default     = "web"
   description = "Type of Application Insights (Web/Other)"
+}
+
+variable "common_tags" {
+  type = map(string)
 }
