5ff024c8a1a429c9427661a7d9bae7a3e6e21984
david.crespo <david.crespo@hmcts.net>
2021-02-08 15:07:03 +0000

    shared infra changes to subscribe to the CCD topic

--- servicebus.tf
+++ servicebus.tf
@@ -1,38 +1,19 @@
 // Azure service bus
 locals {
-  topic_name        = "${var.product}-case-event-topic-${var.env}"
-  subscription_name = "${var.product}-case-event-subscription-${var.env}"
-  servicebus_namespace_name       = "${var.product}-servicebus-${var.env}"
+  topic_name        = "ccd-case-events-${var.env}"
+  subscription_name = "${var.product}-case-events-sub-${var.env}"
+  servicebus_namespace_name       = "ccd-servicebus-${var.env}"
   resource_group_name             = azurerm_resource_group.rg.name
 
 }
 
-//Create namespace
-module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
-  name                = local.servicebus_namespace_name
-  location            = var.location
-  resource_group_name = local.resource_group_name
-  env                 = var.env
-  common_tags         = var.common_tags
-  sku                 = "Premium"
-}
-
-//Create topic
-module "topic" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
-  name                  = local.topic_name
-  namespace_name        = module.servicebus-namespace.name
-  resource_group_name   = local.resource_group_name
-}
-
 //Create subscription
 module "subscription" {
-//  The branch require_session needs to be merged to master
+//fixme:  The branch require_session needs to be merged to master
   source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=require_session"
   name                  = local.subscription_name
-  namespace_name        = module.servicebus-namespace.name
-  topic_name            = module.topic.name
+  namespace_name        = local.servicebus_namespace_name
+  topic_name            = local.topic_name
   resource_group_name   = local.resource_group_name
   requires_session      = true
 }
