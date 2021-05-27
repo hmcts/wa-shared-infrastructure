fadbd65530392e0293a29e1a3c7c8151ed6f3a35
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-05-27 14:56:02 +0100

    add app insights alert for task unconfigured

--- alerts.tf
+++ alerts.tf
@@ -17,3 +17,23 @@ module "wa-exception-alert" {
   resourcegroup_name = azurerm_resource_group.rg.name
   enabled = true
 }
+
+module "wa-camunda-task-unconfigured-exception-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = "${var.location}"
+
+  app_insights_name = "camunda-bpm-appinsights-${var.env}"
+
+  alert_name = "wa-camunda-task-unconfigured-alert"
+  alert_desc = "Triggers when a task could not be configured and it is saved with an unconfigured task state, works with 5 minute poll in camunda-bpm-appinsights-${var.env}."
+  app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and customDimensions[\"Logger Message\"] contains \"Task could not be configured.\" | sort by timestamp desc"
+  custom_email_subject = "Alert: A task could not be configure in wa-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "wa-support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 0
+  resourcegroup_name = azurerm_resource_group.rg.name
+  enabled = true
+}
