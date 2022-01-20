47517aa98f7afcc2da0cd111037b5a879ac3e272
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2022-01-20 16:23:39 +0000

    Conditional filter in aat only (#26)

--- servicebus.tf
+++ servicebus.tf
@@ -6,6 +6,7 @@ locals {
   servicebus_namespace_name         = "ccd-servicebus-${var.env}"
   resource_group_name               = "ccd-shared-${var.env}"
   ccd_case_events_subscription_name = "${var.product}-ccd-case-events-sub-${var.env}"
+  message_context_instances_count   = var.env == "aat" ? 1 : 0
 }
 
 //Create subscription
@@ -41,12 +42,12 @@ resource "azurerm_servicebus_subscription_rule" "allowed_jurisdictions" {
 }
 
 resource "azurerm_servicebus_subscription_rule" "message_context" {
-  count               = var.env == "aat" ? 1 : 0
+  count               = local.message_context_instances_count
   name                = local.subscription_rule_name
   resource_group_name = local.resource_group_name
   namespace_name      = local.servicebus_namespace_name
   topic_name          = local.topic_name
   subscription_name   = local.subscription_name
   filter_type         = "SqlFilter"
-  sql_filter          = "1=1"
+  sql_filter          = "message_context LIKE 'wa-ft%'"
 }
