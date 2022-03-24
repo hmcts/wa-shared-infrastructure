24c34130ecd368277995a8cd8892a24cbdd8b348
oguz akin <akinserdaroguzhan@gmail.com>
2022-03-24 13:16:27 +0000

    task initiation failure alert created

--- alerts.tf
+++ alerts.tf
@@ -37,3 +37,23 @@ module "wa-camunda-task-unconfigured-exception-alert" {
   resourcegroup_name = "camunda-${var.env}"
   enabled = true
 }
+
+module "wa-camunda-task-uninitiated-exception-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = var.location
+
+  app_insights_name = "wa-${var.env}"
+
+  alert_name = "wa-camunda-task-uninitiated-alert"
+  alert_desc = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 60 minute poll in camunda-bpm-appinsights-${var.env}."
+  app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_INITIATION_FAILURES There are some uninitiated tasks\" | sort by timestamp desc"
+  custom_email_subject = "Alert: A task could not be initiated in wa-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "wa-support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 0
+  resourcegroup_name = "camunda-${var.env}"
+  enabled = true
+}
