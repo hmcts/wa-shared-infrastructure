a258e9a06026c1b744dc0fd4bab2a67e21616668
Moussa Kayad <mkayad@gmail.com>
2022-05-04 11:01:04 +0100

    Revert change for RWA-933

--- alerts.tf
+++ alerts.tf
@@ -39,24 +39,3 @@ module "wa-camunda-task-unconfigured-exception-alert" {
   enabled = true
   common_tags = local.common_tags
 }
-
-module "wa-messages-find-problem-messages-alert" {
-  source = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name = "wa-case-event-handler-find-problem-messages-alert"
-  alert_desc = "Triggers when a ccd message is unprocessable state or remains in ready state for more than 1 hour, works with 60 minute poll in case-event-handler-appinsights-${var.env}."
-  app_insights_query = "union traces | where customDimensions[\"LoggingLevel\"] == \"INFO\" and message contains \"FIND_PROBLEM_MESSAGES Retrieved problem messages\" and ( message contains \"UNPROCESSABLE\" or message contains \"READY\") | sort by timestamp desc"
-  custom_email_subject = "Alert: some CCD messages could not be processed in wa-${var.env}"
-  frequency_in_minutes = 60
-  time_window_in_minutes = 60
-  severity_level = "2"
-  action_group_name = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
-  resourcegroup_name =azurerm_resource_group.rg.name
-  common_tags = local.common_tags
-  enabled = true
-}
