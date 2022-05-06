1c0cf7cd7e8c0599a53f8be251789db7c77a941b
TimPickmanHMCTS <101724331+TimPickmanHMCTS@users.noreply.github.com>
2022-05-06 10:56:13 +0100

    RWA-1159 Fix azure tags compliance policy (#37)
    * RWA-1159 Fix azure tags compliance policy
    
    * RWA-1159 tweak to common_tags

--- main.tf
+++ main.tf
@@ -3,12 +3,5 @@ provider "azurerm" {
 }
 
 locals {
-  common_tags = {
-    "environment"  = var.env
-    "Team Name"    = var.team_name
-    "Team Contact" = var.team_contact
-    "Destroy Me"   = var.destroy_me
-  }
-
   managed_identity_object_id = var.managed_identity_object_id
 }
--- resource-group.tf
+++ resource-group.tf
@@ -2,7 +2,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
-  tags     = merge(local.common_tags, map("lastUpdated", timestamp()))
+  tags     = var.common_tags
 }
 
 output "resourceGroup" {
