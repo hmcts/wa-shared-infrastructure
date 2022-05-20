1cc4b50985be2400b5e3d58f4f5ccc761c3c47d6
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-05-20 09:32:31 +0100

    resolve subscription id

--- servicebus.tf
+++ servicebus.tf
@@ -36,7 +36,7 @@ module "ccd_case_event_subscription" {
 resource "azurerm_servicebus_subscription_rule" "allowed_jurisdictions" {
   count               = local.case_events_sub_rule_instances_count
   name                = "${var.product}-case-events-sub-rule-${var.env}"
-  subscription_id     = local.subscription_name
+  subscription_id     = module.subscription.id
   filter_type         = "SqlFilter"
   sql_filter          = "1=1"
 }
@@ -44,7 +44,7 @@ resource "azurerm_servicebus_subscription_rule" "allowed_jurisdictions" {
 resource "azurerm_servicebus_subscription_rule" "message_context" {
   count               = local.message_context_instances_count
   name                = "${var.product}-message-context-sub-rule-${var.env}"
-  subscription_id     = local.subscription_name
+  subscription_id     = module.subscription.id
   filter_type         = "SqlFilter"
   sql_filter          = "message_context LIKE 'wa-ft%'"
 }
