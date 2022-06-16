5a516445392e2bf354f04a7c99eddcba23268644
Andrei Trushkov <andrei.trushkov@version1.com>
2022-06-16 16:19:58 +0100

    RWA-1154: added app_insights_connection_string needed for Appinsights v3 upgrade

--- application-insights.tf
+++ application-insights.tf
@@ -26,8 +26,15 @@ output "appInsightsInstrumentationKey" {
   value = azurerm_application_insights.appinsights.instrumentation_key
 }
 
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.wa_key_vault.key_vault_id
+}
+
 resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
   name         = "AppInsightsInstrumentationKey"
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.wa_key_vault.key_vault_id
 }
+
