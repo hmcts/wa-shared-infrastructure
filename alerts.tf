9ed649308343617a0c9b7571dfc59b13a016530b
Serdar Oğuzhan AKIN <42956817+serdaroguzhanakin@users.noreply.github.com>
2022-06-29 08:59:54 +0100

    RWA-1100 TASK_INITIATION_FAILURES and TASK_TERMINATION_FAILURES alerts (#30)
    * task initiation failure alert created
    
    * TASK_TERMINATION_FAILURES alert added
    
    * allow new build
    
    * common_tags was added
    
    * merge master

--- alerts.tf
+++ alerts.tf
@@ -15,8 +15,8 @@ module "wa-exception-alert" {
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold = 0
   resourcegroup_name = azurerm_resource_group.rg.name
-  enabled = true
   common_tags = var.common_tags
+  enabled = true
 }
 
 module "wa-camunda-task-unconfigured-exception-alert" {
@@ -36,8 +36,50 @@ module "wa-camunda-task-unconfigured-exception-alert" {
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold = 0
   resourcegroup_name = "camunda-${var.env}"
-  enabled = true
   common_tags = var.common_tags
+  enabled = true
+}
+
+module "wa-camunda-task-uninitiated-exception-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = var.location
+
+  app_insights_name = "wa-${var.env}"
+
+  alert_name = "wa-camunda-task-uninitiated-alert"
+  alert_desc = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 120 minute poll in wa-${var.env}."
+  app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_INITIATION_FAILURES There are some uninitiated tasks\" | sort by timestamp desc"
+  custom_email_subject = "Alert: A task could not be initiated in wa-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "wa-support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 0
+  resourcegroup_name = azurerm_resource_group.rg.name
+  common_tags = var.common_tags
+  enabled = true
+}
+
+module "wa-camunda-task-unterminated-exception-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = var.location
+
+  app_insights_name = "wa-${var.env}"
+
+  alert_name = "wa-camunda-task-unterminated-alert"
+  alert_desc = "Triggers when a task could not be terminated, works with 120 minute poll in wa-${var.env}."
+  app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_TERMINATION_FAILURES There are some unterminated tasks\" | sort by timestamp desc"
+  custom_email_subject = "Alert: A task could not be terminated in wa-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "wa-support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 0
+  resourcegroup_name = azurerm_resource_group.rg.name
+  common_tags = var.common_tags
+  enabled = true
 }
 
 module "wa-messages-find-problem-messages-alert" {
