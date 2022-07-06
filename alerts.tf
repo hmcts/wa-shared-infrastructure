68f991753d4260909b3f26e08ae2a74150efa605
Serdar Oğuzhan AKIN <42956817+serdaroguzhanakin@users.noreply.github.com>
2022-07-06 14:12:05 +0100

    change frequency of initiation and termination failure alerts (#41)

--- alerts.tf
+++ alerts.tf
@@ -50,8 +50,8 @@ module "wa-camunda-task-uninitiated-exception-alert" {
   alert_desc = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 120 minute poll in wa-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_INITIATION_FAILURES There are some uninitiated tasks\" | sort by timestamp desc"
   custom_email_subject = "Alert: A task could not be initiated in wa-${var.env}"
-  frequency_in_minutes = 5
-  time_window_in_minutes = 5
+  frequency_in_minutes = 60
+  time_window_in_minutes = 60
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
@@ -71,8 +71,8 @@ module "wa-camunda-task-unterminated-exception-alert" {
   alert_desc = "Triggers when a task could not be terminated, works with 120 minute poll in wa-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_TERMINATION_FAILURES There are some unterminated tasks\" | sort by timestamp desc"
   custom_email_subject = "Alert: A task could not be terminated in wa-${var.env}"
-  frequency_in_minutes = 5
-  time_window_in_minutes = 5
+  frequency_in_minutes = 60
+  time_window_in_minutes = 60
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
