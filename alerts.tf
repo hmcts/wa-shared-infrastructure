2d3331add6bdf3ef65619d3897174362a7f17472
Moussa Kayad <mkayad@gmail.com>
2023-02-22 14:46:37 +0000

    Change variable type for alerts declared variables

--- alerts.tf
+++ alerts.tf
@@ -8,12 +8,12 @@ module "wa-exception-alert" {
   alert_desc = "Triggers when a message falls into the Dead Letter Queue, works with 5 minute poll in wa-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and customDimensions[\"Logger Message\"] contains \"dead lettered\" | sort by timestamp desc"
   custom_email_subject = "Alert: Message was dead-lettered in wa-${var.env}"
-  frequency_in_minutes = 5
-  time_window_in_minutes = 5
+  frequency_in_minutes = "5"
+  time_window_in_minutes = "5"
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
+  trigger_threshold = "0"
   resourcegroup_name = azurerm_resource_group.rg.name
   common_tags = var.common_tags
   enabled = true
@@ -29,12 +29,12 @@ module "wa-camunda-task-uninitiated-exception-alert" {
   alert_desc = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 120 minute poll in wa-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_INITIATION_FAILURES There are some uninitiated tasks\" | sort by timestamp desc"
   custom_email_subject = "Alert: A task could not be initiated in wa-${var.env}"
-  frequency_in_minutes = 60
-  time_window_in_minutes = 60
+  frequency_in_minutes = "60"
+  time_window_in_minutes = "60"
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
+  trigger_threshold = "0"
   resourcegroup_name = azurerm_resource_group.rg.name
   common_tags = var.common_tags
   enabled = true
@@ -50,12 +50,12 @@ module "wa-camunda-task-unterminated-exception-alert" {
   alert_desc = "Triggers when a task could not be terminated, works with 120 minute poll in wa-${var.env}."
   app_insights_query = "union traces, exceptions | where customDimensions[\"LoggingLevel\"] == \"WARN\" and message contains \"TASK_TERMINATION_FAILURES There are some unterminated tasks\" | sort by timestamp desc"
   custom_email_subject = "Alert: A task could not be terminated in wa-${var.env}"
-  frequency_in_minutes = 60
-  time_window_in_minutes = 60
+  frequency_in_minutes = "60"
+  time_window_in_minutes = "60"
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
+  trigger_threshold = "0"
   resourcegroup_name = azurerm_resource_group.rg.name
   common_tags = var.common_tags
   enabled = true
@@ -71,12 +71,12 @@ module "wa-messages-find-problem-messages-alert" {
   alert_desc = "Triggers when a ccd message is unprocessable state or remains in ready state for more than 1 hour, works with 60 minute poll in case-event-handler-appinsights-${var.env}."
   app_insights_query = "union traces | where customDimensions[\"LoggingLevel\"] == \"INFO\" and message contains \"FIND_PROBLEM_MESSAGES Retrieved problem messages\" and ( message contains \"UNPROCESSABLE\" or message contains \"READY\") | sort by timestamp desc"
   custom_email_subject = "Alert: some CCD messages could not be processed in wa-${var.env}"
-  frequency_in_minutes = 60
-  time_window_in_minutes = 60
+  frequency_in_minutes = "60"
+  time_window_in_minutes = "60"
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
+  trigger_threshold = "0"
   resourcegroup_name =azurerm_resource_group.rg.name
   common_tags = var.common_tags
   enabled = true
@@ -92,12 +92,12 @@ module "wa-cft-task-reconfiguration-exception-alert" {
   alert_desc = "Triggers when a task could not be reconfigured for a defined time in task-management-api-appinsights-${var.env}."
   app_insights_query = "union traces | where customDimensions[\"LoggingLevel\"] == \"INFO\" and message contains \"Task Execute Reconfiguration Failed\" | sort by timestamp desc"
   custom_email_subject = "Alert: some tasks could not be reconfigured in wa-${var.env}"
-  frequency_in_minutes = 60
-  time_window_in_minutes = 60
+  frequency_in_minutes = "60"
+  time_window_in_minutes = "60"
   severity_level = "2"
   action_group_name = "wa-support"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold = 0
+  trigger_threshold = "0"
   resourcegroup_name =azurerm_resource_group.rg.name
   common_tags = var.common_tags
   enabled = true
