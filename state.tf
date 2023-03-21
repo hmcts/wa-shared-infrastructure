7c82be59b6580f33b6c16bd681150ca65658ef1c
vasu <vasu.kaveri@hmcts.net>
2023-03-21 18:16:24 +0000

    RWA-2359: update azuread version and terraform version

--- state.tf
+++ state.tf
@@ -4,14 +4,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.30.0"
+      version = "~> 3.48.0"
     }
     random = {
       source = "hashicorp/random"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.30.0"
+      version = "2.36.0"
     }
   }
 }
