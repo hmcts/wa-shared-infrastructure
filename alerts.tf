3c67bab134f5ec0d49e43c81cf6b7ca56cb22a24
lars <lars.lunde@hmcts.net>
2025-11-07 14:46:37 +0000

    Changing naming

--- alerts.tf
+++ alerts.tf
@@ -66,7 +66,7 @@ module "tm-camunda-task-uninitiated-exception-slack-alert" {
   location = var.location
 
   app_insights_name          = "wa-${var.env}"
-  alert_name                 = "wa-camunda-task-uninitiated-slack-alert"
+  alert_name                 = "tm-camunda-task-uninitiated-exception-slack-alert"
   alert_desc                 = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 60 minute poll in wa-${var.env}"
   app_insights_query         = "traces | project timestamp, msg=message | union (exceptions | project timestamp, msg=outerMessage) | where msg has_all (\"TASK_INITIATION_FAILURES There are some uninitiated tasks\") | parse kind=relaxed msg with * \"created: \" created_iso \"Z\" * | extend created_str = strcat(created_iso, \"Z\") | extend created = todatetime(created_str) | where isnotempty(created) and created >= ago(1h) | project timestamp, created, msg"
   custom_email_subject       = "Alert: A task could not be initiated in wa-${var.env}"
@@ -108,7 +108,7 @@ module "tm-camunda-task-unterminated-exception-slack-alert" {
 
   app_insights_name = "wa-${var.env}"
 
-  alert_name                 = "wa-camunda-task-unterminated-alert"
+  alert_name                 = "tm-camunda-task-unterminated-exception-slack-alert"
   alert_desc                 = "Triggers when a task could not be terminated, works with 120 minute poll in wa-${var.env}."
   app_insights_query         = "union traces, exceptions | where message contains \"TASK_TERMINATION_FAILURES There are some unterminated tasks\" | sort by timestamp desc"
   custom_email_subject       = "Alert: A task could not be terminated in wa-${var.env}"
@@ -150,7 +150,7 @@ module "tm-messages-find-problem-messages-slack-alert" {
 
   app_insights_name = "wa-${var.env}"
 
-  alert_name                 = "wa-case-event-handler-find-problem-messages-alert"
+  alert_name                 = "tm-messages-find-problem-messages-slack-alert"
   alert_desc                 = "Triggers when a ccd message is unprocessable state or remains in ready state for more than 1 hour, works with 60 minute poll in case-event-handler-appinsights-${var.env}."
   app_insights_query         = "union traces | where message contains \"FIND_PROBLEM_MESSAGES Retrieved problem messages\" and ( message contains \"UNPROCESSABLE\" or message contains \"READY\") | sort by timestamp desc"
   custom_email_subject       = "Alert: some CCD messages could not be processed in wa-${var.env}"
@@ -186,13 +186,13 @@ module "wa-cft-task-reconfiguration-exception-alert" {
   enabled                    = true
 }
 
-module "tm-cft-task-reconfiguration-exception-slack-alert" {
+module "tm-api-task-reconfiguration-exception-slack-alert" {
   source   = "git@github.com:hmcts/cnp-module-metric-alert"
   location = var.location
 
   app_insights_name = "wa-${var.env}"
 
-  alert_name                 = "wa-task-management-api-reconfiguration-exception-alert"
+  alert_name                 = "tm-api-task-reconfiguration-exception-slack-alert"
   alert_desc                 = "Triggers when a task could not be reconfigured for a defined time in task-management-api-appinsights-${var.env}."
   app_insights_query         = "union traces | where message contains \"Task Execute Reconfiguration Failed\" | sort by timestamp desc"
   custom_email_subject       = "Alert: some tasks could not be reconfigured in wa-${var.env}"
@@ -234,7 +234,7 @@ module "tm-task-replication-problem-slack-alert" {
 
   app_insights_name = "wa-${var.env}"
 
-  alert_name                 = "wa-task-management-api-replication-problem-alert"
+  alert_name                 = "tm-task-replication-problem-slack-alert"
   alert_desc                 = "Triggers when a task could not be replicated in task-management-api-appinsights-${var.env}."
   app_insights_query         = "union traces | where message contains \"TASK_REPLICATION_ERROR: \" | sort by timestamp desc"
   custom_email_subject       = "Alert: some tasks could not be replicated in wa-${var.env}"
