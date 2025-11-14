ffea652a9820d05ae4c46153b084fb50979a7db1
lars <lars.lunde@hmcts.net>
2025-11-14 15:34:54 +0000

    Enable slack alerts

--- alerts.tf
+++ alerts.tf
@@ -19,27 +19,6 @@ module "wa-exception-alert" {
   enabled                    = true
 }
 
-module "wa-camunda-task-uninitiated-exception-alert" {
-  source   = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name                 = "wa-camunda-task-uninitiated-alert"
-  alert_desc                 = "Triggers when a task could not be initiated and it is saved with an unconfigured task state, works with 120 minute poll in wa-${var.env}."
-  app_insights_query         = "union traces, exceptions | where message contains \"TASK_INITIATION_FAILURES There are some uninitiated tasks\" | sort by timestamp desc"
-  custom_email_subject       = "Alert: A task could not be initiated in wa-${var.env}"
-  frequency_in_minutes       = "60"
-  time_window_in_minutes     = "60"
-  severity_level             = "2"
-  action_group_name          = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = "0"
-  resourcegroup_name         = azurerm_resource_group.rg.name
-  common_tags                = var.common_tags
-  enabled                    = true
-}
-
 module "tm-camunda-task-uninitiated-exception-alert-slack-summary" {
   source   = "git@github.com:hmcts/cnp-module-metric-alert"
   location = var.location
@@ -58,7 +37,7 @@ module "tm-camunda-task-uninitiated-exception-alert-slack-summary" {
   trigger_threshold          = "0"
   resourcegroup_name         = azurerm_resource_group.rg.name
   common_tags                = var.common_tags
-  enabled                    = false
+  enabled                    = var.enable-tm-slack-alert
 }
 
 module "tm-camunda-task-uninitiated-exception-slack-alert" {
@@ -79,27 +58,6 @@ module "tm-camunda-task-uninitiated-exception-slack-alert" {
   resourcegroup_name         = azurerm_resource_group.rg.name
   common_tags                = var.common_tags
   enabled                    = var.enable-tm-slack-alert
-}
-
-module "wa-camunda-task-unterminated-exception-alert" {
-  source   = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name                 = "wa-camunda-task-unterminated-alert"
-  alert_desc                 = "Triggers when a task could not be terminated, works with 120 minute poll in wa-${var.env}."
-  app_insights_query         = "union traces, exceptions | where message contains \"TASK_TERMINATION_FAILURES There are some unterminated tasks\" | sort by timestamp desc"
-  custom_email_subject       = "Alert: A task could not be terminated in wa-${var.env}"
-  frequency_in_minutes       = "60"
-  time_window_in_minutes     = "60"
-  severity_level             = "2"
-  action_group_name          = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = "0"
-  resourcegroup_name         = azurerm_resource_group.rg.name
-  common_tags                = var.common_tags
-  enabled                    = true
 }
 
 module "tm-camunda-task-unterminated-exception-slack-alert" {
@@ -123,27 +81,6 @@ module "tm-camunda-task-unterminated-exception-slack-alert" {
   enabled                    = var.enable-tm-slack-alert
 }
 
-module "wa-messages-find-problem-messages-alert" {
-  source   = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name                 = "wa-case-event-handler-find-problem-messages-alert"
-  alert_desc                 = "Triggers when a ccd message is unprocessable state or remains in ready state for more than 1 hour, works with 60 minute poll in case-event-handler-appinsights-${var.env}."
-  app_insights_query         = "union traces | where message contains \"FIND_PROBLEM_MESSAGES Retrieved problem messages\" and ( message contains \"UNPROCESSABLE\" or message contains \"READY\") | sort by timestamp desc"
-  custom_email_subject       = "Alert: some CCD messages could not be processed in wa-${var.env}"
-  frequency_in_minutes       = "60"
-  time_window_in_minutes     = "60"
-  severity_level             = "2"
-  action_group_name          = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = "0"
-  resourcegroup_name         = azurerm_resource_group.rg.name
-  common_tags                = var.common_tags
-  enabled                    = true
-}
-
 module "tm-messages-find-problem-messages-slack-alert" {
   source   = "git@github.com:hmcts/cnp-module-metric-alert"
   location = var.location
@@ -165,27 +102,6 @@ module "tm-messages-find-problem-messages-slack-alert" {
   enabled                    = var.enable-tm-slack-alert
 }
 
-module "wa-cft-task-reconfiguration-exception-alert" {
-  source   = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name                 = "wa-task-management-api-reconfiguration-exception-alert"
-  alert_desc                 = "Triggers when a task could not be reconfigured for a defined time in task-management-api-appinsights-${var.env}."
-  app_insights_query         = "union traces | where message contains \"Task Execute Reconfiguration Failed\" | sort by timestamp desc"
-  custom_email_subject       = "Alert: some tasks could not be reconfigured in wa-${var.env}"
-  frequency_in_minutes       = "60"
-  time_window_in_minutes     = "60"
-  severity_level             = "2"
-  action_group_name          = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = "0"
-  resourcegroup_name         = azurerm_resource_group.rg.name
-  common_tags                = var.common_tags
-  enabled                    = true
-}
-
 module "tm-api-task-reconfiguration-exception-slack-alert" {
   source   = "git@github.com:hmcts/cnp-module-metric-alert"
   location = var.location
@@ -205,27 +121,6 @@ module "tm-api-task-reconfiguration-exception-slack-alert" {
   resourcegroup_name         = azurerm_resource_group.rg.name
   common_tags                = var.common_tags
   enabled                    = var.enable-tm-slack-alert
-}
-
-module "wa-task-replication-problem-alert" {
-  source   = "git@github.com:hmcts/cnp-module-metric-alert"
-  location = var.location
-
-  app_insights_name = "wa-${var.env}"
-
-  alert_name                 = "wa-task-management-api-replication-problem-alert"
-  alert_desc                 = "Triggers when a task could not be replicated in task-management-api-appinsights-${var.env}."
-  app_insights_query         = "union traces | where message contains \"TASK_REPLICATION_ERROR: \" | sort by timestamp desc"
-  custom_email_subject       = "Alert: some tasks could not be replicated in wa-${var.env}"
-  frequency_in_minutes       = "60"
-  time_window_in_minutes     = "60"
-  severity_level             = "2"
-  action_group_name          = "wa-support"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = "0"
-  resourcegroup_name         = azurerm_resource_group.rg.name
-  common_tags                = var.common_tags
-  enabled                    = true
 }
 
 module "tm-task-replication-problem-slack-alert" {
--- prod.tfvars
+++ prod.tfvars
@@ -1,2 +1,3 @@
 allowed_jurisdictions                                     = "'ia', 'IA','civil', 'CIVIL','privatelaw', 'PRIVATELAW','publiclaw', 'PUBLICLAW','employment','EMPLOYMENT','sscs','SSCS','ST_CIC','st_cic'"
 enable-wa-task-management-api-task-deletion-failure-alert = true
+enable-tm-slack-alert = true
