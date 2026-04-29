cc8408b928e57540c15e0302f9538045de6a7b9f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-29 13:46:33 +0100

    DTSPO-30520 - Add jenkins access to keyvault (#151)

--- key-vault.tf
+++ key-vault.tf
@@ -9,6 +9,7 @@ module "wa_key_vault" {
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = var.wa_product_group_object_id
 
@@ -16,5 +17,10 @@ module "wa_key_vault" {
   common_tags                          = var.common_tags
   create_managed_identity              = true
   additional_managed_identities_access = var.additional_managed_identities_access
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
